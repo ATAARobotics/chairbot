37a606e03c0e321765d74fa2d2bea2df3a5f9021
THATW33B <Nappa.pakka@gmail.com>
2019-01-20 15:54:41 -0700

    Adjusted code in preperation for driver assist code.

--- src/main/java/ca/fourthreethreefour/Robot.java
+++ src/main/java/ca/fourthreethreefour/Robot.java
@@ -7,6 +7,8 @@
 
 package ca.fourthreethreefour;
 
+import java.awt.Image;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -16,6 +18,7 @@ import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 import edu.wpi.cscore.CvSink;
@@ -60,8 +63,8 @@ public class Robot extends TimedRobot implements Constants
     //TODO use below values when driver assit code is ready to be added
     //private double centerX = 0.0;
     //private double centerY = 0.0;
-    private List<Rect> rectList;
-	//private final Object imgLock = new Object();
+    private List<Rect> rectList = new LinkedList<Rect>();
+	private final Object imgLock = new Object();
 
     // Ultrasonic goes here
 
@@ -127,40 +130,108 @@ public class Robot extends TimedRobot implements Constants
         //Starts GripPipeline
         GripPipeline visionProcessing = new GripPipeline();
 
+        //Initializes new Rect array to send to autonomous periodic for tracking
+        Rect[] visionTarget = new Rect[2];
+
         //Configures vision Thread
         visionThread = new VisionThread(camera, visionProcessing, pipeline -> {
+            //If we want to stream camera feed while no countours are present, remove below comment and comment in else statement below
+            //cvSink.grabFrame(source)
 
             //Check if the final output has anything
             if (!pipeline.filterContoursOutput().isEmpty()) {
                 //Grabs frame for processing
                 cvSink.grabFrame(source);
-                rectList.clear();
-                try{
+                //TODO try catch statement is commented out. This will not be removed until tests prove it's not used
+                //try{
                     //Processes Image
                     visionProcessing.process(source);
 
                     //Creates All Rectangles
-                    for(int i = 0; i <= pipeline.filterContoursOutput().size(); i++){
+                    for(int i = 0; i < pipeline.filterContoursOutput().size(); i++){
                         Rect rx = Imgproc.boundingRect(pipeline.filterContoursOutput().get(i));
 
                         //Prints Location of Rectangle
                         System.out.println("Object " + i + ": " + rx.toString());
-                        
-                        //Draw Rectangle
-                        Imgproc.rectangle(source, new Point(rx.x, rx.y), new Point(rx.x + rx.width, rx.y + rx.height), new Scalar(0,0,255), 2);
 
-                        //Adds rectangle to array to store size and position values
+                        //Adds rectangle to List to store size and position values
                         rectList.add(rx);
                     }
+                    //Sets up Indices For rectangle index holding
+                    int largestIndex = 0;
+                    int secondIndex = 0;
 
-                    //Send Frame
-                    outputStream.putFrame(source);
+                    //If there is only one target, make our Vision target two of our same rectangle
+                    if(rectList.size() == 1){
+                        visionTarget[0] = rectList.get(0);
+                        visionTarget[1] = visionTarget[0];
+                    }
+                    //Removes Rectangles from List only if there's more than two rectangles
+                    else if(rectList.size() > 2){
+                    //Determines the two largest rectangles and indexes them
+                    for (int i = 1; i < rectList.size();i++){
+                        //If the current rectangle is larger than our largest
+                        if(rectList.get(largestIndex).area()<rectList.get(i).area()){
+                            visionTarget[1] = visionTarget[0];
+                            secondIndex = largestIndex;
+                            visionTarget[0] = rectList.get(i);
+                            largestIndex = i;
+                        }
+                        //If the current rectangle is larger thanm the second largest
+                        else if(rectList.get(secondIndex).area()<rectList.get(i).area()){
+                            visionTarget[1] = rectList.get(i);
+                            secondIndex = i;
+                        }
+                    }
+                    //Remove rectangles except for ones at our indexes
+                    /*for(int i = 0; i < rectList.size();i++){
+                        //If index tries to find third Largest rectangle, break
+                        if(i == 2){
+                            break;
+                        }
+                        //if our index hits one of our targets, draw the target
+                        if(i == largestIndex ||i == secondIndex){
+                            //Draws rectangle if it matches with either index
+                            Imgproc.rectangle(source, new Point(rectList.get(i).x, rectList.get(i).y), new Point(rectList.get(i).x + rectList.get(i).width, rectList.get(i).y + rectList.get(i).height), new Scalar(0,0,255), 2);
+                        }
+                        //if all checks fail,
+                        else{
+                            //Finds rectangle
+                            Rect r = rectList.get(i);
+                            //Removes Rectangle from List
+                            rectList.remove(r);
+                            //Decrements all of our counters
+                            largestIndex--;
+                            secondIndex--;
+                            i--;
+                        }
+                    }*/
+                }
+                //TODO Remove below when done debugging
 
-                } catch (IndexOutOfBoundsException | NullPointerException e){
+                //Draws rectangles
+                Imgproc.rectangle(source, new Point(visionTarget[0].x, visionTarget[0].y), new Point(visionTarget[0].x + visionTarget[0].width, visionTarget[0].y + visionTarget[0].height), new Scalar(0,0,255), 2);
+                Imgproc.rectangle(source, new Point(visionTarget[1].x, visionTarget[1].y), new Point(visionTarget[1].x + visionTarget[1].width, visionTarget[1].y + visionTarget[1].height), new Scalar(0,0,255), 2);
+
+                //Send Frame
+                outputStream.putFrame(source);
+                
+                //Sends data
+                synchronized(imgLock){
+                    
+                }
+
+                //Clear list
+                rectList.clear();
+
+            //} 
+            /*catch (IndexOutOfBoundsException | NullPointerException e){
+                    //failsafe
                     System.out.println("No vision target detected " + e.getMessage());
                     outputStream.putFrame(source);
-                }
+                }*/
             } else {
+                //outputStream.putFrame(source);
                 System.out.println("No Contours Detected");
             }
         });
