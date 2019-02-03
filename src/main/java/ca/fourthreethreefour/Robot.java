15d945ccfe3690bb789c2bbb5a932967f72f276f
CelestialTater <coledewis@gmail.com>
2019-02-03 01:22:28 +0000

    Moved vision into seperate class

--- src/main/java/ca/fourthreethreefour/Robot.java
+++ src/main/java/ca/fourthreethreefour/Robot.java
@@ -53,19 +53,10 @@ public class Robot extends TimedRobot implements Constants
     private SpeedControllerGroup leftSideDriveMotors;
     private SpeedControllerGroup rightSideDriveMotors;
     private DifferentialDrive robotDrive;
-    
-    // Vision Proccessing
-    private static final int IMG_WIDTH = 320;
-    private static final int IMG_HEIGHT = 240;
-    private VisionThread visionThread;
-    private double centerX = 0.0;
-    Rect[] visionTarget = new Rect[2];
 
+    private VisionAlignment visionCamera;
     
-    //TODO use below values when driver assit code is ready to be added
-    //private double centerX = 0.0;
-    private List<Rect> rectList = new LinkedList<Rect>();
-	private final Object imgLock = new Object();
+   
     
     // Ultrasonic goes here
     
@@ -95,9 +86,7 @@ public class Robot extends TimedRobot implements Constants
     NetworkTableEntry RIGHT_DRIVE_MOTOR_ENTRY = portsTab.addPersistent("Right Drive Motor", 3).getEntry();
     int RIGHT_DRIVE_MOTOR = (int) RIGHT_DRIVE_MOTOR_ENTRY.getDouble(3);
     
-    //Create Item on Shuffleboard to Adjust Camera Exposure
-    NetworkTableEntry CAMERAEXPOSURE_ENTRY = dynamicSettingsTab.addPersistent("Camera Exposure", 0).getEntry();
-    int CAMERAEXPOSURE = (int) CAMERAEXPOSURE_ENTRY.getDouble(50);
+   
     
     @Override
     public void robotInit()
@@ -114,162 +103,9 @@ public class Robot extends TimedRobot implements Constants
         leftSideDriveMotors = new SpeedControllerGroup(leftDriveMotor);
         rightSideDriveMotors = new SpeedControllerGroup(rightDriveMotor);
         robotDrive = new DifferentialDrive(leftSideDriveMotors, rightSideDriveMotors);
-        
-        // Initialize Camera with properties
-        UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-        camera.setResolution(IMG_WIDTH, IMG_HEIGHT);
-        
-        //Initializes image Mat for modification
-        Mat source = new Mat();
-        
-        //Starts CvSink to capture Mats
-        CvSink cvSink = CameraServer.getInstance().getVideo();
-        
-        //TODO remove once debugging is done
-        CvSource outputStream = CameraServer.getInstance().putVideo("Image Analysis", IMG_WIDTH, IMG_HEIGHT);
-        
-        //Makes GripPipeline Object
-        GripPipeline visionProcessing = new GripPipeline();
-        
-        
-        //Configures vision Thread
-        visionThread = new VisionThread(camera, visionProcessing, pipeline -> {
 
-            //Sets Camera Exposure with value from Shuffleboard
-            CAMERAEXPOSURE = (int) CAMERAEXPOSURE_ENTRY.getDouble(50);
-            //System.out.println(CAMERAEXPOSURE);
-            camera.setExposureManual(CAMERAEXPOSURE);
+        visionCamera = new VisionAlignment();
 
-            //Grabs frame for processing
-            cvSink.grabFrame(source);
-            
-            //Initializes new Rect array to store data for assist code
-            Rect placeHolder = new Rect(0, 0, 1, 1);
-            visionTarget[0] = placeHolder;
-            visionTarget[1] = visionTarget[0];
-
-            //TODO test if this is is necessary
-            //Processes Image
-            visionProcessing.process(source);
-
-            //If filter has nothing, send frame
-            if (!pipeline.filterContoursOutput().isEmpty()) {
-                //Grabs frame for processing
-                cvSink.grabFrame(source);
-                //TODO try catch statement is commented out. This will not be removed until tests prove it's not used
-                //try{
-                    //Processes Image
-                    visionProcessing.process(source);
-
-                    //Creates All Rectangles
-                    for(int i = 0; i < pipeline.filterContoursOutput().size(); i++){
-                        Rect rx = Imgproc.boundingRect(pipeline.filterContoursOutput().get(i));
-
-                        //Prints Location of Rectangle
-                        System.out.println("Object " + i + ": " + rx.toString());
-
-                        //Adds rectangle to List to store size and position values
-                        rectList.add(rx);
-                    }
-                    //Sets up Indices For rectangle index holding
-                    int largestIndex = 0;
-                    int secondIndex = 0;
-                    
-                    if(rectList.size() == 2){
-                        visionTarget[0] = rectList.get(0);
-                        visionTarget[1] = rectList.get(1);
-                    }
-                    //If there is only one target, make our Vision target two of our same rectangle
-                    if(rectList.size() == 1){
-                        visionTarget[0] = rectList.get(0);
-                        visionTarget[1] = visionTarget[0];
-                    }
-                    //Removes Rectangles from List only if there's more than two rectangles
-                    else if(rectList.size() > 2){
-                    //Determines the two largest rectangles and indexes them
-                    for (int i = 1; i < rectList.size();i++){
-                        //If the current rectangle is larger than our largest
-                        if(rectList.get(largestIndex).area()<rectList.get(i).area()){
-                            visionTarget[1] = visionTarget[0];
-                            secondIndex = largestIndex;
-                            visionTarget[0] = rectList.get(i);
-                            largestIndex = i;
-                        }
-                        //If the current rectangle is larger thanm the second largest
-                        else if(rectList.get(secondIndex).area()<rectList.get(i).area()){
-                            visionTarget[1] = rectList.get(i);
-                            secondIndex = i;
-                        }
-                    }
-                    //Remove rectangles except for ones at our indexes
-                    /*for(int i = 0; i < rectList.size();i++){
-                        //If index tries to find third Largest rectangle, break
-                        if(i == 2){
-                            break;
-                        }
-                        //if our index hits one of our targets, draw the target
-                        if(i == largestIndex ||i == secondIndex){
-                            //Draws rectangle if it matches with either index
-                            Imgproc.rectangle(source, new Point(rectList.get(i).x, rectList.get(i).y), new Point(rectList.get(i).x + rectList.get(i).width, rectList.get(i).y + rectList.get(i).height), new Scalar(0,0,255), 2);
-                        }
-                        //if all checks fail,
-                        else{
-                            //Finds rectangle
-                            Rect r = rectList.get(i);
-                            //Removes Rectangle from List
-                            rectList.remove(r);
-                            //Decrements all of our counters
-                            largestIndex--;
-                            secondIndex--;
-                            i--;
-                        }
-                    }*/
-                }
-                //TODO Remove below when done debugging
-
-                //Draws rectangles
-                Imgproc.rectangle(source, new Point(visionTarget[0].x, visionTarget[0].y), new Point(visionTarget[0].x + visionTarget[0].width, visionTarget[0].y + visionTarget[0].height), new Scalar(0,0,255), 2);
-                Imgproc.rectangle(source, new Point(visionTarget[1].x, visionTarget[1].y), new Point(visionTarget[1].x + visionTarget[1].width, visionTarget[1].y + visionTarget[1].height), new Scalar(0,0,255), 2);
-
-                //Send Frame
-                outputStream.putFrame(source);
-                
-                //Sends data
-                synchronized(imgLock){
-                    
-                }
-
-                //Clear list
-                rectList.clear();
-
-            //} 
-            /*catch (IndexOutOfBoundsException | NullPointerException e){
-                    //failsafe
-                    System.out.println("No vision target detected " + e.getMessage());
-                    outputStream.putFrame(source);
-                }*/
-            } else {
-                outputStream.putFrame(source);
-                System.out.println("No Contours Detected");
-            }
-            //TODO Remove below when done debugging
-            //Draws rectangles
-            Imgproc.rectangle(source, new Point(visionTarget[0].x, visionTarget[0].y), new Point(visionTarget[0].x + visionTarget[0].width, visionTarget[0].y + visionTarget[0].height), new Scalar(0,0,255), 2);
-            Imgproc.rectangle(source, new Point(visionTarget[1].x, visionTarget[1].y), new Point(visionTarget[1].x + visionTarget[1].width, visionTarget[1].y + visionTarget[1].height), new Scalar(0,0,255), 2);
-            //Send Frame
-            outputStream.putFrame(source);
-            
-            //Sends data
-            synchronized(imgLock){
-                
-            }
-        });
-        
-        //starts vision thread
-        visionThread.start(); 
-        
-        //drive = new RobotDrive(1, 2);
-        
         // Sets the appropriate configuration settings for the motors
         leftSideDriveMotors.setInverted(true);
         rightSideDriveMotors.setInverted(true);
@@ -290,20 +126,7 @@ public class Robot extends TimedRobot implements Constants
     @Override
     public void autonomousPeriodic()
     {
-        double centerX;
-        synchronized (imgLock) {
-            centerX = visionTarget[0].x + (visionTarget[0].width / 2);
-        }
-        if (visionTarget[0] == null){
-            return;
-        }else {
-            double turn = 0;
-            turn = centerX - (IMG_WIDTH / 2);
-            System.out.println(turn);
-            robotDrive.arcadeDrive(-0.6, turn * 0.005);
-        }
-        //double turn = centerX - (IMG_WIDTH / 2);
-       
+       visionCamera.align();
     }
     
     @Override
