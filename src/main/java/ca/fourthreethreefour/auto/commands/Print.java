5b95124491b033c2882003c0598fcae8b7ca826d
Kulkinz <cool@coolspages.com>
2019-12-29 12:08:08 -0700

    Added testable autofile

--- src/main/java/ca/fourthreethreefour/auto/commands/Print.java
+++ src/main/java/ca/fourthreethreefour/auto/commands/Print.java
@@ -1,7 +1,5 @@
 package ca.fourthreethreefour.auto.commands;
 
-import java.util.Vector;
-
 import edu.wpi.first.wpilibj.command.InstantCommand;
 
 public class Print extends InstantCommand {
@@ -10,14 +8,6 @@ public class Print extends InstantCommand {
 
     public Print(String str) {
         this.str = str;
-    }
-
-    public Print(Vector<String[]> commandArgs) {
-        try {
-            str = commandArgs.get(0)[0];
-        } catch (Exception e) {
-            str = "Test";
-        }
     }
 
     public void execute() {
